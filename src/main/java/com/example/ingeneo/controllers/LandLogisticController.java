0b5f3f08ea00c32dfc5169a3a792b280ace47f7a
juliansarmien18 <julian010422@gmail.com>
2022-03-06 15:23:37 -0500

    creacion servicios, repositorios y controladores

---
+++ src/main/java/com/example/ingeneo/controllers/LandLogisticController.java
@@ -0,0 +1,5 @@
+package com.example.ingeneo.controllers;
+
+public class LandLogisticController {
+    
+}
---
+++ src/main/java/com/example/ingeneo/repositories/ClientProductLogisticRepository.java
@@ -0,0 +1,5 @@
+package com.example.ingeneo.repositories;
+
+public class ClientProductLogisticRepository {
+    
+}
